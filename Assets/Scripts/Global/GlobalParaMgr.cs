c564e87a3a3e5a6e73aec887e8bdc91d4acc492f
1993229653 <1993229653@qq.com>
2024-07-09 15:00:03 +0800

    开始页面、异步加载到登录界面

---
+++ Assets/Scripts/Global/GlobalParaMgr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//跨场景全局数值传递
+namespace Global
+{
+
+    public static class GlobalParaMgr 
+    {
+        //下一个场景名称
+        public static ScenesEnum NextScenesName = ScenesEnum.LongonScenes;
+        
+    }
+}
+
---
+++ Assets/Scripts/Global/GlobalParameter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//定义整个项目枚举类型
+//定义整个项目委托，系统常量
+namespace Global
+{
+
+    public class GlobalParameter 
+    {
+
+       
+
+
+    }
+    public enum ScenesEnum
+    {
+        StartScenes,
+        LoadingScenes,
+        LongonScenes,
+        LevelOne,
+        LevelTwo,
+        BaseScenes
+    }
+}
